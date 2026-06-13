5d498fbfda403cf91c682fd2f4f79a79e032e500
diwinigah <winidorc1@gmail.com>
2026-06-13 23:17:22 +0000

    ajout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,7 @@
         <!-- Logo -->
         <a href="https://jeunesseenjesus.org" class="j2-logo">
             <img
-                src="{{ asset('images/logo.svg') }}"
+                src="{{ asset('images/logo.png') }}"
                 alt="J²"
                 class="j2-logo-img"
                 onerror="this.style.display='none'">
