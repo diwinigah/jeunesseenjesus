9d0b5616f8acceaffcd2a5862f653f5f6c974714
diwinigah <winidorc1@gmail.com>
2026-06-14 00:11:16 +0000

    ajout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <link rel="icon"
         type="image/png"
-        href="{{ asset('images/logo.png') }}">
+        href="{{ asset('images/logo1.png') }}">
     <link rel="shortcut icon"
         type="image/png"
-        href="{{ asset('images/logo.png') }}">
+        href="{{ asset('images/logo1.png') }}">
     <link rel="apple-touch-icon"
         href="{{ asset('images/logo.png') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -36,7 +36,7 @@
         <!-- Logo -->
         <a href="https://jeunesseenjesus.org" class="j2-logo">
             <img
-                src="{{ asset('images/logo.png') }}"
+                src="{{ asset('images/logo1.png') }}"
                 alt="J²"
                 class="j2-logo-img"
                 onerror="this.style.display='none'">
@@ -117,7 +117,7 @@
             <h3 class="j2-footer-title">Site officiel</h3>
             <p>Pour plus d'informations sur nos activités :</p>
             <a href="https://jeunesseenjesus.org" target="_blank" class="j2-btn-orange">
-                Visiter jeunesseenjesus.org
+               Accueil
             </a>
         </div>
     </div>
